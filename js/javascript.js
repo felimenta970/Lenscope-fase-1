5a6c347876cbbbd78bab7b2edee366e3fc416aea
Felipe Pimenta <felimenta97@gmail.com>
2020-10-05 17:44:13 -0300

    Comentários

--- js/javascript.js
+++ js/javascript.js
@@ -6,10 +6,12 @@ A lente Prime só pode ser uma opção para o usuário que tiver grau esférico,
 OBS: O grau limite que o usuário pode inserir é de 0 a -15 para esférico e -6 para cilíndrico e são números iterados de 0,25 em 0,25 (ex: -0,25; 0; +0,25; etc) *
 */
 
+// Arredonda para o 0.25 mais próximo
 function arredonda(num) {
     return Math.round(num*4)/4;
 }
 
+// Funções para mostrar o resultado
 function isPrime(){
     document.getElementById("isPrime").style.display = "block";
 }
@@ -18,9 +20,11 @@ function isVision() {
     document.getElementById("isVision").style.display = "block";
 }
 
+// Função principal para verificar qual lente
 function verificaLente() {
     let esqEsferico, dirEsferico, esqCilindrico, dirCilindrico;
 
+    // Obtemos os valores colocados
     esqEsferico = arredonda(document.getElementById("esqEsferico").value);
     dirEsferico = arredonda(document.getElementById("dirEsferico").value);
 
@@ -28,22 +32,29 @@ function verificaLente() {
     esqCilindrico = arredonda(document.getElementById("esqCilindrico").value);
     dirCilindrico = arredonda(document.getElementById("dirCilindrico").value);
 
+
     if (esqCilindrico == 0 && dirCilindrico == 0){
+        // Se não tem grau no cilíndrico
         if (esqEsferico >= -12 && esqEsferico <= -3 && dirEsferico >= -12 && dirEsferico <= -3) {
+            // Caso seja no espaço entre -3 e -12, temos lentes Prime
             isPrime();
         }
 
         if (esqEsferico >= -3 || dirEsferico >= -3) {
+            // Se tivermos menos que -3 no Esférico, lentes Vision
             isVision();
         }
 
     } else {
 
         if (esqCilindrico >= -2 && dirCilindrico >= -2) {
+            // Se for até -2 no Cilindrico
             if (esqEsferico >= -10 && esqEsferico <= -3 && dirEsferico >= -10 && dirEsferico <= -3) {
+                // Se for entre -3 e -10 no esférico, Prime
                 isPrime();
 
             } else {
+                // Se não, Vision
                 isVision();
             }
 
@@ -51,6 +62,7 @@ function verificaLente() {
 
         else {
             if (esqCilindrico >= -5 && dirCilindrico >= -5) {
+                // Se for outro valor, Vision
                 isVision();
             }
         }
